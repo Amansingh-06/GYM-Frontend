34a03a518cc9c6eb12b31aa62e0f062571ebb08d
Amansingh-06 <amansingh.as9140@gmail.com>
2024-12-09 10:53:33 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GYM-Frontend/', // This should match your repository name
+  base: '/GYM-Frontend/', // Replace 'GYM-Frontend' with your GitHub repository name
 });
